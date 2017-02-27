ec20d1458427a7b441c884c99346dec629de1810
Grant Evans <grant.evans300@gmail.com>
2017-02-27 14:22:02 -0800

    finished add task, eidt task, and mark to complete

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,9 +1,10 @@
-//variables for list and logout button
+//variables for lists and logout button
 var todoList = document.getElementById("todoList");
+var doneList = document.getElementById("doneList");
 var logoutButton = document.getElementById("logout");
 
 //variable declaration for initial task creation
-var addTaskButton = document.getElementById("myModal");
+var addTaskButton = document.getElementById("addTask");
 var taskForm = document.getElementById("task-form");
 var taskDescriptionInput = document.getElementById("description-input");
 var taskDateInput = document.getElementById("date-input");
@@ -18,6 +19,7 @@ var editTaskDateInput = document.getElementById("edit-date-input");
 var editTaskTitleInput = document.getElementById("edit-title-input");
 var editTaskTimeInput = document.getElementById("edit-time-input");
 var editTaskPriorityInput = document.getElementById("edit-priority");
+var editTaskSubmitButton = document.getElementById("editSubmit");
 
 //tenetive error
 var textError = document.getElementById("post-error");
@@ -31,6 +33,7 @@ firebase.auth().onAuthStateChanged(function(user) {
     if(user) {
         var database = firebase.database();
         var todo = database.ref("channels/todo");
+        var done = database.ref("channels/completed")
 
         todo.on("child_added", function(data) {
             var id = data.key;
@@ -64,7 +67,21 @@ firebase.auth().onAuthStateChanged(function(user) {
             editButton.className = "btn btn-link";
             editButton.innerText = "Edit";
             editButton.type = "click";
-            ;
+            
+            //reassign the id of the submit button from the modal to be specific to each node
+
+
+            var editSubmitButton = document.createElement("button");
+            editSubmitButton.id = (id + "editSubmit");
+            editSubmitButton.type = "click"
+            editSubmitButton.className = "btn btn-primary";
+            editSubmitButton.innerText = "Update Text";
+            //id and functionality
+ 
+            var editModalFooter = document.getElementById("edit-modal-footer");
+            
+
+            //editModalFooter.appendChild(editSubmitButton);
 
             var removeButton = document.createElement("button");
             removeButton.id = "remove-button";
@@ -73,77 +90,165 @@ firebase.auth().onAuthStateChanged(function(user) {
 
             var editTaskModal = document.createElement("modal");
 
+             // Completed button
+            var doneButton = document.createElement("button");
+            doneButton.id = "done-button";
+            doneButton.className = "btn btn-primary";
+            doneButton.innerText = "Mark as Complete";
+            doneButton.type = "click";
+
+            // move to firebase completed and delete from todo firebase
+            doneButton.addEventListener("click", function(e) {
+                var user = firebase.auth().currentUser;
+                node = database.ref("channels/todo/" + id);
+
+                done.push({
+                    uid: user.uid,
+                    displayName: user.displayName,
+                    description: document.getElementById(id + "description").innerHTML,
+                    date: document.getElementById(id + "date").innerHTML,
+                    title: document.getElementById(id + "title").innerHTML,
+                    time: document.getElementById(id + "time").innerHTML,
+                    priority: document.getElementById(id + "priority")
+                })
+                .then(function() {
+                    node.remove();
+                })
+                .catch(function(error) {
+                    textError.innerText = "Error completing your task";
+                    textError.classList.add("active");
+                })
+            });
+                
+                var todoLi = document.createElement("li");
+
+                // Append to the to-do list item
+                todoLi.id =  id;
+                todoLi.className = "list-group-item";
+                todoLi.appendChild(title);
+                todoLi.appendChild(description);
+                todoLi.appendChild(date);
+                todoLi.appendChild(time);
+                todoLi.appendChild(priority);
+                todoLi.appendChild(editButton);
+                todoLi.appendChild(removeButton);
+                todoLi.appendChild(doneButton);
+
+            
 
             //Toggle edit area with edit button
             editButton.addEventListener("click", function(e) {
                 $('#editModal').modal('show');
-                editTaskTitleInput.value = item.title;
-                console.log(item.description);
-                editTaskDescriptionInput.value = item.description;
-                editTaskDateInput.value = item.date;
-                editTaskTimeInput.value = item.time;
-                editTaskPriorityInput.value = item.priority;
+                editTaskTitleInput.value = document.getElementById(id + "title").innerHTML;
+                editTaskDescriptionInput.value = document.getElementById(id + "description").innerHTML;
+                editTaskDateInput.value = document.getElementById(id + "date").innerHTML;
+                editTaskTimeInput.value = document.getElementById(id + "time").innerHTML;
+                editTaskPriorityInput.value = document.getElementById(id + "priority").innerHTML;
+                
+
+
+                var editSubmitButtonId = editSubmitButton.id.substring(0, editSubmitButton.id.length - 10);
+
+                console.log(id) 
+                console.log(editSubmitButtonId);
+
+                if(id === editSubmitButtonId) {
+                    while(editModalFooter.hasChildNodes()) {
+                        editModalFooter.removeChild(editModalFooter.lastChild)
+                    }
+                    editModalFooter.appendChild(editSubmitButton);
+                }
+                
             });
 
-            editTaskForm.addEventListener("submit", function(e) {
-                node = database.red("channels/todo/" + id);
 
-                    var editTitleRaw = editTaskTitleInput.value;
-                    var editDescriptionRaw = editTaskDescriptionInput.value;
-                    var editDateRaw = editTaskDateInput.value;
-                    var editTimeRaw = editTaskTimeInput.value;
-                    var editPriorityRaw =editTaskPriorityInput.value;
-
+            //change the node within Firebase
+            editSubmitButton.addEventListener("click", function(e) {
+                node = database.ref("channels/todo/" + id);
                 node.set({
                     uid: user.uid,
                     displayName: user.displayName,
                     description: editTaskDescriptionInput.value,
-                    date: editDateRaw,
-                    title: editTitleRaw,
-                    time: editTimeRaw,
-                    priority: editPriorityRaw
+                    date: editTaskDateInput.value,
+                    title: editTaskTitleInput.value,
+                    time: editTaskTimeInput.value,
+                    priority: editTaskPriorityInput.value
+                });
 
-                })
-            })
+                console.log("changes submitted to firebase");
 
-            // Remove button function and confirmation message
+                $('#editModal').modal('hide');             
+            });
+
+            // Remove button and confirmation message. Removes from firebase
             removeButton.addEventListener("click", function(e) {
                 if (confirm("Are you sure you want to delete this task?") == true){
-                    node = database.ref("channels/general/" + id);
+                    console.log("delete" + id);
+                    node = database.ref("channels/todo/" + id);
                     node.remove();
                 }   
             });
 
-            var todoLi = document.createElement("li");
+            // Append to the HTML
+            todoList.appendChild(todoLi);  
 
-            todoLi.className = "list-group-item";
-            todoLi.appendChild(title);
-            todoLi.appendChild(description);
-            todoLi.appendChild(date);
-            todoLi.appendChild(time);
-            todoLi.appendChild(priority);
-            todoLi.appendChild(editButton);
-            todoLi.appendChild(removeButton);
-            
-
-            todoList.appendChild(todoLi);
-
-            
+            console.log(id + "child added");          
         });
 
-        todo.on("child_changed", function(data) {
+        done.on("child_added", function(data) {
             var id = data.key;
             var item = data.val();
             
-            document.getElementById(id + "description")
+            //create done list item
+
+            var title = document.createElement("h4");
+            title.id = (id + "title");
+            title.innerText = item.title;
+
+            var description = document.createElement("p");
+            description.id = (id + "description");
+            description.innerText = item.description;
+
+            var doneLi = document.createElement("li");
+
+            doneLi.className = ("list-group-item");
+            doneLi.appendChild(title);
+            doneLi.appendChild(description);
+
+            doneList.appendChild(doneLi);
         });
+
+        // Change the DOM when firebase is changed
+        todo.on("child_changed", function(data) {
+
+            var id = data.key;
+            var item = data.val();
+            
+            document.getElementById(id + "title").innerHTML = item.title;
+            document.getElementById(id + "description").innerHTML = item.description;
+            document.getElementById(id + "date").innerHTML = item.date;
+            document.getElementById(id + "time").innerHTML = item.time;
+            document.getElementById(id + "priority").innerHTML = item.priority;
+
+            console.log(id);
+            console.log("DOM changes when todo firebase is changed");
+        });
+
+        // Change the DOM when firebase is removed
+        todo.on("child_removed", function(data) {
+            var id = data.key;
+
+            var removeNode = document.getElementById(id);
+            removeNode.parentNode.removeChild(removeNode);
+        });
+
     } else {
         window.location.href = "index.html";
     }
 
 })
 
-taskForm.addEventListener("submit", function(e) {
+addTaskButton.addEventListener("click", function(e) {
     console.log("submit button hit");
     var database = firebase.database();
     var todo = database.ref("channels/todo");
@@ -166,11 +271,18 @@ taskForm.addEventListener("submit", function(e) {
         priority: priorityRaw
     })
     .then(function() {
+        document.getElementById("title-input").value = "";
         document.getElementById("description-input").value = "";
+        document.getElementById("date-input").value = "";
+        document.getElementById("time-input").value = "";
+        document.getElementById("priority").value = "";
+         $('#addTaskModal').modal('hide');
     })
     .catch(function(error) {
         textError.innerText = "Error creating your task";
         textError.classList.add("active");
-    })
+    });
+
+    //$('#myModal').modal('show');
 });
 
